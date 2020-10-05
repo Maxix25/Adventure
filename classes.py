5b67e336858a98af5f05700422a1d6997195bbff
Maxix25 <68287079+Maxix25@users.noreply.github.com>
2020-10-05 07:54:30 -0300

    Fix indentation errors

--- classes.py
+++ classes.py
@@ -2,32 +2,33 @@ import time
 from funciones import printf
 class Player:
     def __init__(self, attack, defense, hp):
-        self.attack = attack
-        self.defense = defense
-        self.hp = hp
-        self.exp = 0
+		self.attack = attack
+		self.defense = defense
+		self.hp = hp
+		self.exp = 0
+		
     def fight(self, enemy):
-        self.enemy = enemy
-		while True:
-			self.damage_dealt = self.damage - enemy.defense
-			self.wins = False
-			enemy.wins = False
-			enemy.hp -= self.damage_dealt
-			printf("You dealt", self.damage_dealt)
-			if enemy.hp <= 0:
-				printf("You win!")
-				self.wins = True
-				return self.wins
-				break
-			printf("Enemy has", enemy.hp, "health")
-			time.sleep(1)
-			enemy.damage_dealt = enemy.damage - self.defense
-			self.hp -= enemy.damage_dealt
-			printf("The enemy attacks!, enemy dealt you", enemy.damage_dealt, "damage")
-			if self.hp <= 0:
-				printf("You lose...")
-				enemy.wins = True
-				return enemy.wins
-				break
-			printf("You have", self.hp, "health")
-			time.sleep(1)
+		self.enemy = enemy
+	while True:
+		self.damage_dealt = self.damage - enemy.defense
+		self.wins = False
+		enemy.wins = False
+		enemy.hp -= self.damage_dealt
+		printf("You dealt", self.damage_dealt)
+		if enemy.hp <= 0:
+			printf("You win!")
+			self.wins = True
+			return self.wins
+			break
+		printf("Enemy has", enemy.hp, "health")
+		time.sleep(1)
+		enemy.damage_dealt = enemy.damage - self.defense
+		self.hp -= enemy.damage_dealt
+		printf("The enemy attacks!, enemy dealt you", enemy.damage_dealt, "damage")
+		if self.hp <= 0:
+			printf("You lose...")
+			enemy.wins = True
+			return enemy.wins
+			break
+		printf("You have", self.hp, "health")
+		time.sleep(1)
