cc717cf29cfd3083cda90524f85f9a830bfe7817
Maxix25 <maximiliano.besoain@gmail.com>
2020-10-05 11:42:28 -0300

    New classes

--- classes.py
+++ classes.py
@@ -1,7 +1,9 @@
 import time
-from funciones import printf
+from functions import printf
+turn = 0
+items_list = []
 class Player:
-	def __init__(self, attack, defense, hp):
+	def __init__(self, attack:int, defense:int, hp:int):
 		self.attack = attack
 		self.defense = defense
 		self.hp = hp
@@ -9,26 +11,35 @@ class Player:
 		
 	def fight(self, enemy):
 		self.enemy = enemy
-		while True:
-			self.damage_dealt = self.damage - enemy.defense
+		printf("You got yourself into a battle, what would you like to do?")
+		try:
+			move = int(input(
+				"[1] Attack\n",
+				"[2] Use item"
+				))
+		except ValueError:
+			print("You introduced a non-valid option")
+		if move == 1:
+			self.damage_dealt = self.attack - enemy.defense
 			self.wins = False
 			enemy.wins = False
 			enemy.hp -= self.damage_dealt
-			printf("You dealt", self.damage_dealt)
+			printf(f"You dealt {self.damage_dealt} damage")
 			if enemy.hp <= 0:
 				printf("You win!")
 				self.wins = True
 				return self.wins
 				break
-			printf("Enemy has", enemy.hp, "health")
+			printf(f"Enemy has {enemy.hp} health")
 			time.sleep(1)
-			enemy.damage_dealt = enemy.damage - self.defense
-			self.hp -= enemy.damage_dealt
-			printf("The enemy attacks!, enemy dealt you", enemy.damage_dealt, "damage")
-			if self.hp <= 0:
-				printf("You lose...")
-				enemy.wins = True
-				return enemy.wins
-				break
-			printf("You have", self.hp, "health")
-			time.sleep(1)
+		elif move == 2:
+			printf("Choose your item:")
+			time.sleep(0.5)
+			print(items_list)
+	def print_state(self):
+		print(
+			"You have", self.hp, "hp",
+			"You have", self.defense, "defense"
+			"You have", self.attack, "attack"
+			"You have", self.exp, "exp"
+		)
