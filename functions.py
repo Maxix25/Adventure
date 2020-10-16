9998889f74473237037d00de5202e64406b87958
Maxix25 <maximiliano.besoain@gmail.com>
2020-10-16 00:39:39 -0300

    Discomment printf and add the option function

--- functions.py
+++ functions.py
@@ -6,5 +6,18 @@ def printf(sentence: str):
     for char in sentence:
         sys.stdout.write(char)
         sys.stdout.flush()
-        # time.sleep(0.05)
+        time.sleep(0.07)
     print()
+def option(range2):
+	while True:
+		try:
+			choice = int(input("Introduce your option: "))
+		except ValueError:
+			print("You introduced a non-valid character, try again")
+			continue
+		if choice > range2:
+			print("You introduced a non-valid number, try again")
+			continue
+		break
+	return choice
+
