1416a15d87be1740007f853d58e49faa39bceac9
Maxix25 <maximiliano.besoain@gmail.com>
2020-10-16 00:39:10 -0300

    More methods

--- classes.py
+++ classes.py
@@ -1,45 +1,111 @@
 import time
-from functions import printf
-turn = 0
-items_list = []
-class Player:
-	def __init__(self, attack:int, defense:int, hp:int):
-		self.attack = attack
+import random
+from functions import *
+turn = random.randrange(1,3)
+player_wins = None
+items_list = ["Potion"]
+moves = ["Attack"]
+class Character:
+	def __init__(self, damage:int, defense:int, hp:int):
+		self.damage = damage
 		self.defense = defense
 		self.hp = hp
 		self.exp = 0
 		
 	def fight(self, enemy):
-		self.enemy = enemy
-		printf("You got yourself into a battle, what would you like to do?")
-		try:
-			move = int(input(
-				"[1] Attack\n",
-				"[2] Use item"
-				))
-		except ValueError:
-			print("You introduced a non-valid option")
-		if move == 1:
-			self.damage_dealt = self.attack - enemy.defense
-			self.wins = False
-			enemy.wins = False
-			enemy.hp -= self.damage_dealt
-			printf(f"You dealt {self.damage_dealt} damage")
-			if enemy.hp <= 0:
-				printf("You win!")
-				self.wins = True
-				return self.wins
+		global turn
+		global player_wins
+		print("You enter into a fight!")
+		self.print_state()
+		print("---------------------------------------")
+		enemy.enemy_state()	
+		input("Press ENTER to continue...")
+
+		def player_turn():
+			global turn
+			global player_wins
+			printf("It's your turn!, what would you like to do?")
+			print(
+				"[1] Attack",
+				"[2] Use Item"
+				)
+			choice = option(2)
+			while True:
+				if choice == 1:
+					global turn
+					global player_wins
+					self.damage_dealt = self.damage - enemy.defense
+					enemy.hp -= self.damage_dealt
+					printf(f"You dealt {self.damage_dealt} damage!")
+					if enemy.hp <= 0:
+						global player_wins
+						player_wins = True
+					else:
+						enemy.enemy_state()
+				else:
+					back = False
+					counter = 0
+					object = 1
+					for i in items_list:
+						print(f"[{object}] {items_list[counter]}")
+						counter += 1
+						object += 1
+					print(f"[{object}] Back")
+					print("What item do you want to use?")
+					choice = option(object)
+					if choice == object:
+						back = True
+						break
+					else:
+						items_list.pop(choice - 1)
+				if back == True:
+					continue
+				turn = 2
 				break
-			printf(f"Enemy has {enemy.hp} health")
-			time.sleep(1)
-		elif move == 2:
-			printf("Choose your item:")
-			time.sleep(0.5)
-			print(items_list)
+		def enemy_turn():
+			global turn
+			printf("It's the enemy's turn")
+			action = random.randrange(1,3)
+			if action == 1:
+				global turn
+				printf("The enemy attacks!")
+				enemy.damage_dealt = enemy.damage - self.defense
+				self.hp -= enemy.damage_dealt
+				time.sleep(1)
+				printf(f"The enemy dealt {enemy.damage_dealt} damage")
+				if self.hp <= 0:
+					global player_wins
+					player_wins = False
+				else:	
+					self.print_state()
+			else:
+				printf("The enemy uses an item!")
+				time.sleep(1)
+			turn = 1
+		# Check the character turn
+		while self.hp > 0 and enemy.hp > 0:
+			if turn == 1:
+				player_turn()
+			elif turn == 2:
+				enemy_turn()
+			elif self.hp <= 0 and enemy.hp <= 0:
+				# Check who won the fight
+				if player_wins == True:
+					printf("You win!")
+					break
+				else:
+					printf("You lost...")
+					break
 	def print_state(self):
 		print(
 			"You have", self.hp, "hp",
-			"You have", self.defense, "defense"
-			"You have", self.attack, "attack"
-			"You have", self.exp, "exp"
+			"\nYou have", self.defense, "defense"
+			"\nYou deal", self.damage, "damage"
+			"\nYou have", self.exp, "exp"
+		)
+	def enemy_state(enemy):
+		print(
+			"The enemy has", enemy.hp, "hp",
+			"\nThe enemy has", enemy.defense, "defense"
+			"\nThe enemy deals", enemy.damage, "damage"
 		)
